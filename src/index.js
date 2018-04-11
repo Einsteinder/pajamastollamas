23b36067b334c6ff7202fe09653dd4048e490544
einsteinder <einsteinder@gmail.com>
2018-04-11 15:40:41 -0400

    set up layout

--- src/index.js
+++ src/index.js
@@ -1,31 +1,53 @@
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
+import { Navbar,Nav,NavItem,NavDropdown,MenuItem } from 'react-bootstrap';
+
 const { Header, Content, Footer } = Layout;
+
 
 ReactDOM.render(
   <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={['2']}
-        style={{ lineHeight: '64px' }}
-      >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ padding: '0 50px' }}>
+  
+    <Navbar inverse collapseOnSelect>
+  <Navbar.Header>
+    <Navbar.Brand>
+      <a href="#brand">Pajamas to Llamas</a>
+    </Navbar.Brand>
+    <Navbar.Toggle />
+  </Navbar.Header>
+  <Navbar.Collapse>
+    <Nav>
+      <NavItem eventKey={1} href="#">
+        Link
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Link
+      </NavItem>
+      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+        <MenuItem eventKey={3.1}>Action</MenuItem>
+        <MenuItem eventKey={3.2}>Another action</MenuItem>
+        <MenuItem eventKey={3.3}>Something else here</MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.3}>Separated link</MenuItem>
+      </NavDropdown>
+    </Nav>
+    <Nav pullRight>
+      <NavItem eventKey={1} href="#">
+        Login
+      </NavItem>
+      <NavItem eventKey={2} href="#">
+        Sign up
+      </NavItem>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+<Content style={{ padding: '0 50px' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>Home</Breadcrumb.Item>
         <Breadcrumb.Item>List</Breadcrumb.Item>
@@ -33,6 +55,7 @@ ReactDOM.render(
       </Breadcrumb>
       <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
     </Content>
+
     <Footer style={{ textAlign: 'center' }}>
       Pajamas to Llamas ©2018 Created by Cowboys of Gilead
     </Footer>
