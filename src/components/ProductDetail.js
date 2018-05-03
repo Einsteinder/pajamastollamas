660acf310baa3fd5a52053a1d312c45ea46c23f2
einsteinder <einsteinder@gmail.com>
2018-05-03 16:51:26 -0400

    claar after submit but stupid

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -4,121 +4,132 @@ import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { Navbar,Nav,NavItem,NavDropdown,MenuItem } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Button, Comment, Form, Header } from 'semantic-ui-react'
+import { Button, Comment, Form, Header ,Ref} from 'semantic-ui-react'
 
 const {  Content, Footer } = Layout;
 
 class ProductDetail extends Component {
 
-    state={
-        currentUser:"u1",
-        textarea:"",
-        products:[{
-            imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/165250/1000x1000/51810_MN.jpg",
-            name:"Loving Paws",
-            price:10,
-            id:"p1",
-            reviews:[{
-                id:"r1",
-                userId:"u1",
-                content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
-                timestamp:"February 8, 2018",
-                rate:4
+    constructor(props) {
+        super(props);
+        this.textInput = React.createRef();
+        this.handleClick = this.handleClick.bind(this);
+
+
+        this.state ={
+            node:"",
+            currentUser:"u1",
+            textarea:"",
+            products:[{
+                imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/165250/1000x1000/51810_MN.jpg",
+                name:"Loving Paws",
+                price:10,
+                id:"p1",
+                reviews:[{
+                    id:"r1",
+                    userId:"u1",
+                    content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
+                    timestamp:"February 8, 2018",
+                    rate:4
+                },
+                {
+                    id:"r2",
+                    userId:"u2",
+                    content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
+                    timestamp:"January 13, 2018",
+                    rate:5
+                }
+            ]
             },
             {
-                id:"r2",
-                userId:"u2",
-                content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
-                timestamp:"January 13, 2018",
-                rate:5
-            }
-        ]
-        },
-        {
-            imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/191733/1000x1000/58818_Crop1.jpg",
-            name:"All Over Paws Plush Lounge Pants",
-            price:11,
-            id:"p2",
-            reviews:[{
-                id:"r3",
-                userId:"u2",
-                content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
-                timestamp:"February 8, 2018",
-                rate:4
+                imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/191733/1000x1000/58818_Crop1.jpg",
+                name:"All Over Paws Plush Lounge Pants",
+                price:11,
+                id:"p2",
+                reviews:[{
+                    id:"r3",
+                    userId:"u2",
+                    content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
+                    timestamp:"February 8, 2018",
+                    rate:4
+                },
+                {
+                    id:"r4",
+                    userId:"u2",
+                    content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
+                    timestamp:"January 13, 2018",
+                    rate:5
+                }
+            ]
             },
             {
-                id:"r4",
-                userId:"u2",
-                content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
-                timestamp:"January 13, 2018",
-                rate:5
-            }
-        ]
-        },
-        {
-            imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/190414/1000x1000/83200_MN.jpg",
-            name:"Super Cozy™ Sherbet Pets Slipper Booties",
-            price:12,
-            id:"p3",
-            reviews:[{
-                id:"r5",
-                userId:"u1",
-                content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
-                timestamp:"February 8, 2018",
-                rate:4
+                imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/190414/1000x1000/83200_MN.jpg",
+                name:"Super Cozy™ Sherbet Pets Slipper Booties",
+                price:12,
+                id:"p3",
+                reviews:[{
+                    id:"r5",
+                    userId:"u1",
+                    content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
+                    timestamp:"February 8, 2018",
+                    rate:4
+                },
+                {
+                    id:"r6",
+                    userId:"u3",
+                    content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
+                    timestamp:"January 13, 2018",
+                    rate:5
+                }
+            ]
             },
             {
-                id:"r6",
-                userId:"u3",
-                content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
-                timestamp:"January 13, 2018",
-                rate:5
-            }
-        ]
-        },
-        {
-            imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/194651/1000x1000/26498_LAB.jpg",
-            name:"Purple Paw Women's Casual Shorts",
-            price:15,
-            id:"p4",
-            reviews:[{
-                id:"r7",
-                userId:"u1",
-                content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
-                timestamp:"February 8, 2018",
-                rate:4
+                imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/194651/1000x1000/26498_LAB.jpg",
+                name:"Purple Paw Women's Casual Shorts",
+                price:15,
+                id:"p4",
+                reviews:[{
+                    id:"r7",
+                    userId:"u1",
+                    content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
+                    timestamp:"February 8, 2018",
+                    rate:4
+                },
+                {
+                    id:"r8",
+                    userId:"u2",
+                    content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
+                    timestamp:"January 13, 2018",
+                    rate:5
+                }
+            ]
             },
             {
-                id:"r8",
-                userId:"u2",
-                content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
-                timestamp:"January 13, 2018",
-                rate:5
-            }
-        ]
-        },
-        {
-            imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/190623/1000x1000/85382_MN.jpg",
-            name:"Rainbow Paws Thermal Long Sleeve Top",
-            price:5,
-            id:"p5",
-            reviews:[{
-                id:"r9",
-                userId:"u1",
-                content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
-                timestamp:"February 8, 2018",
-                rate:4
-            },
-            {
-                id:"r10",
-                userId:"u3",
-                content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
-                timestamp:"January 13, 2018",
-                rate:5
-            }
-        ]
-        }]
-    }
+                imgURL:"https://d39rqydp4iuyht.cloudfront.net/store/product/190623/1000x1000/85382_MN.jpg",
+                name:"Rainbow Paws Thermal Long Sleeve Top",
+                price:5,
+                id:"p5",
+                reviews:[{
+                    id:"r9",
+                    userId:"u1",
+                    content:"Love these pjs. Warm, comfortable and wonderful for lounging or sleeping. Highly recommend.",
+                    timestamp:"February 8, 2018",
+                    rate:4
+                },
+                {
+                    id:"r10",
+                    userId:"u3",
+                    content:"Bought these as a gift for my mother-in-law. She loved them. Says they're soft & comfy & keep her warm. She is always cold at night but not anymore. I have purchased several items and am pleased with them all.",
+                    timestamp:"January 13, 2018",
+                    rate:5
+                }
+            ]
+            }]
+        }
+    ;
+      }
+      handleRef = node => this.setState({ node })
+
 
     handleChangeText = event => {
         this.setState({ textarea: event.target.value });
@@ -142,8 +153,10 @@ class ProductDetail extends Component {
         }]}):newProducts.push(product))
         this.setState({products:newProducts})
         this.setState({textarea:""})
-        this.refs.textInput.value=" "
-
+        
+        this.state.node.children[0].value=""
+        console.log(this.state.node.children[0].value
+)
     }
   render() {
 
@@ -216,7 +229,11 @@ class ProductDetail extends Component {
 
 
     <Form reply>
+    <Ref innerRef={this.handleRef}>
+
       <Form.TextArea onChange={this.handleChangeText} />
+      </Ref>
+
       <Button onClick={this.handleClick }content='Add Comment' labelPosition='left' icon='edit' primary />
     </Form>
   </Comment.Group>
