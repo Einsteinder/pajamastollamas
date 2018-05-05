3e1e749ce10dc2ab556de8cf52309cc8c4d3caa0
einsteinder <einsteinder@gmail.com>
2018-05-05 14:47:30 -0400

    signin signup

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import Button from 'antd/lib/button';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import { Popover,Layout, Menu, Breadcrumb } from 'antd';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Login from './Login';
+import Signup from './Signup';
 
 const { Header, Content, Footer } = Layout;
 
 class AppLayout extends Component {
+    state = {
+        login_visible: false,
+        signup_visible: false,
+
+      }
+      hidesignin = () => {
+        this.setState({
+            signin_visible: false,
+        });
+      }
+      handleVisibleChange = (login_visible) => {
+        this.setState({ login_visible });
+      }
+      handleSingUpVisibleChange = (signup_visible) => {
+        this.setState({ signup_visible });
+      }
     render() {
         return (
             <div className="App">
@@ -33,12 +51,29 @@ class AppLayout extends Component {
                           
                             </Nav>
                             <Nav pullRight>
-                                <NavItem eventKey={1} href="#">
+                            <Popover
+        content={<Login/>}
+        title="Title"
+        trigger="click"
+        visible={this.state.login_visible}
+        onVisibleChange={this.handleVisibleChange}
+      >
+      <NavItem eventKey={1} href="#">
                                     Login
     </NavItem>
-                                <NavItem eventKey={2} href="#">
+      </Popover>
+      <Popover
+        content={<Signup/>}
+        title="Title"
+        trigger="click"
+        visible={this.state.signup_visible}
+        onVisibleChange={this.handleSingUpVisibleChange}
+      >
+      <NavItem eventKey={2} href="#">
                                     Sign up
     </NavItem>
+      </Popover>       
+                         
                             </Nav>
                         </Navbar.Collapse>
                     </Navbar>
