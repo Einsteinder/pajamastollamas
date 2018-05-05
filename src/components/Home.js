6690427024e5d0bf8e6651aa6ac18b939d4f8033
einsteinder <einsteinder@gmail.com>
2018-05-05 16:03:53 -0400

    change home pic order

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,19 +11,20 @@ class Home extends Component {
             <AppLayout content={
                 <Container textAlign='center'>
 
-                <Carousel autoplay>
-    <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/165250/1000x1000/51810_MN.jpg"} alt="home1"/>
-</div>
-    <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/191733/1000x1000/58818_Crop1.jpg"} alt="home2"/>
-</div>
-    <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/190414/1000x1000/83200_MN.jpg"} alt="home3"/>
-</div>
-    <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/194651/1000x1000/26498_LAB.jpg"} alt="home4"/>
-</div>
-  </Carousel>                
-  </Container >
+                    <Carousel autoplay>
+                        <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/191733/1000x1000/58818_Crop1.jpg"} alt="home2" />
+                        </div>
+                        <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/165250/1000x1000/51810_MN.jpg"} alt="home1" />
+                        </div>
 
-                }/>
+                        <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/190414/1000x1000/83200_MN.jpg"} alt="home3" />
+                        </div>
+                        <div><img src={"https://d39rqydp4iuyht.cloudfront.net/store/product/194651/1000x1000/26498_LAB.jpg"} alt="home4" />
+                        </div>
+                    </Carousel>
+                </Container >
+
+            } />
 
         );
     }
